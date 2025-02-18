4fcaea41139e14b9a82d594a1acacdda0401f101
Francisco <gamercrepy8@gmail.com>
2025-02-18 11:54:54 -0600

    updated Modales

--- src/components/Modals.jsx
+++ src/components/Modals.jsx
@@ -1,10 +1,51 @@
+import React from "react";
 
-import React from 'react'
-
-function Moodals() {
+function MoodalDefault(title, description, status) {
   return (
-    <div>Moodals</div>
-  )
+    <div>
+      <div>
+        <div>
+          <h3>{title}</h3>
+          {description && <p>{description}</p>}
+        </div>
+        <button type="button">Aceptar</button>
+      </div>
+    </div>
+  );
 }
 
-export default Moodals
+function MoodalConfirm(title, description, status, confirm) {
+  return (
+    <div>
+      <div>
+        <div>
+          <h3>{title}</h3>
+          {description && <p>{description}</p>}
+        </div>
+        <div>
+          <button type="button">Cancelar</button>
+
+          <button type="button" onClick={confirm}>
+            Confirmar
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function MoodalNotice(title, image, status) {
+  return (
+    <div>
+      <div>
+        <div>
+          <h3>{title}</h3>
+          {image}
+        </div>
+        <button type="button">Aceptar</button>
+      </div>
+    </div>
+  );
+}
+
+export { MoodalDefault, MoodalConfirm, MoodalNotice };
